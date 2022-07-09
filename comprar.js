c822dda0324bc46baf5dcb5c601b7abbad836d94
AdemilsonSimiao <biassistencia@gmail.com>
2022-07-09 15:46:48 +0100

    segundo commit

---
+++ comprar.js
@@ -0,0 +1,11 @@
+const comprar = () => {
+    if (total != ""){
+        window.location.href = "pagamento.html";
+    }
+}
+
+const fPedido = () => {
+    if (total != ""){
+        window.location.href = "confirmacao.html";
+    }
+}
